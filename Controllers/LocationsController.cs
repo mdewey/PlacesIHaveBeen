da5d1da769ff01b9357db677fe1300005083a93a
V <vzmon1@gmail.com>
2018-09-20 11:35:03 -0400

    Added Get / Post function

--- Controllers/LocationsController.cs
+++ Controllers/LocationsController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PlacesTravelled.DataModel;
 
 namespace PlacesTravelled.Controllers {
     [Route ("api/[controller]")]
     [ApiController]
     public class LocationsController : ControllerBase {
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get () {
-            return new List<string> {"Hello", "World"};
+        public IOrderedQueryable<Locations> Get () {
+              var dBConnection = new PlacesTravelledContext();
+
+              var locations = dBConnection.Locations.OrderByDescending(o => o.Date);
+              return locations;
+
+        }//END
+
+        [HttpPost]
+        public string Post ([FromBody] string place){
+
+
+            return "Successful Post "+place;
+
         }
+
+
+
 
     } //END public class LocationsController : ControllerBase
 } //END namespace PlacesTravelled.Controllers
