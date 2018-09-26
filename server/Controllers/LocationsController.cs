b93e77cd83e19ea4e9bc278c231aa41e87d17029
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-26 11:21:51 -0400

    Added DI

--- server/Controllers/LocationsController.cs
+++ server/Controllers/LocationsController.cs
@@ -5,52 +5,63 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PlacesTravelled.DataModel;
 
-namespace PlacesTravelled.Controllers {
-    [Route ("api/[controller]")]
+namespace PlacesTravelled.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class LocationsController : ControllerBase {
-        [HttpGet]
-        public IOrderedQueryable<Locations> Get () {
-              var dBConnection = new PlacesTravelledContext();
+    public class LocationsController : ControllerBase
+    {
 
-              var locations = dBConnection.Locations.OrderBy(o => o.Place.ToLower())
-              .ThenBy(t => t.Date);
-              return locations;
+
+        private PlacesTravelledContext db { get; set; }
+
+        public LocationsController(PlacesTravelledContext _db)
+        { 
+            this.db = _db;
+        }
+
+
+        [HttpGet]
+        public IOrderedQueryable<Locations> Get()
+        {
+            var locations = this.db.Locations.OrderBy(o => o.Place.ToLower())
+            .ThenBy(t => t.Date);
+            return locations;
 
         }//END
 
         [HttpPost]
-        public Locations Post ([FromBody] Locations place){
-            var dbConnection = new PlacesTravelledContext();
-            dbConnection.Locations.Add(place);
-            dbConnection.SaveChanges();
+        public Locations Post([FromBody] Locations place)
+        {
+            this.db.Locations.Add(place);
+            this.db.SaveChanges();
             return place;
-             
+
 
         }//END 
 
         [HttpPatch("{id}")]
-        public Locations Patch (int id) {
+        public Locations Patch(int id)
+        {
             //Find the location inside the database with the Id
-            var dbConnection = new PlacesTravelledContext();
-            var location = dbConnection.Locations.FirstOrDefault(f => f.Id == id);
+            var location = this.db.Locations.FirstOrDefault(f => f.Id == id);
             //Add 1 to TimesVisited
             location.TimesVisited++;
             //Update Time to Visited Now
             location.Date = DateTime.Now;
             //Save it to Database
-            dbConnection.SaveChanges();
+            this.db.SaveChanges();
             //Return new information   
-            return location;         
+            return location;
         }//END
 
         [HttpDelete("{id}")]
-        public ActionResult Delete (int id) {
-            var dbConnection = new PlacesTravelledContext();
-            var location = dbConnection.Locations.FirstOrDefault(f => f.Id == id);
-            dbConnection.Locations.Remove(location);
-            dbConnection.SaveChanges();
-            return Ok(new {success = true });
+        public ActionResult Delete(int id)
+        {
+            var location = this.db.Locations.FirstOrDefault(f => f.Id == id);
+            this.db.Locations.Remove(location);
+            this.db.SaveChanges();
+            return Ok(new { success = true });
         }
 
     } //END public class LocationsController : ControllerBase
